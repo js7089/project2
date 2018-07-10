9e683323b1c01cd37d5b5b1d2c9ee4eb86d71e52
js7089 <38030993+js7089@users.noreply.github.com>
2018-07-10 23:41:01 +0900

    v0.0.1.3
    comments : no changes

--- app/src/main/java/com/example/q/project2/Contacts.java
+++ app/src/main/java/com/example/q/project2/Contacts.java
@@ -46,6 +46,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import org.apache.http.*;
+
+
 import static android.widget.Toast.LENGTH_LONG;
 
 public class Contacts extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
@@ -265,6 +268,48 @@ public class Contacts extends Fragment implements SwipeRefreshLayout.OnRefreshLi
 
         return view;
     }
+
+    /*
+     private String getJSONFormat(String username){
+        String result = "";
+
+        HttpURLConnection con = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+
+        try{
+            URL url = new URL("http://52.231.65.38:5000/" + username);
+            con = (HttpURLConnection) url.openConnection();
+            con.setConnectTimeout(CONN_TIME);
+            con.setReadTimeout(CONN_TIME);
+
+            isr = new InputStreamReader(con.getInputStream());
+            br = new BufferedReader(isr);
+
+            String str = null;
+            while ((str = br.readLine()) != null) {
+                result += str + "\n";
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(con != null){
+                try{con.disconnect();}catch(Exception e){}
+            }
+
+            if(isr != null){
+                try{isr.close();}catch(Exception e){}
+            }
+
+            if(br != null){
+                try{br.close();}catch(Exception e){}
+            }
+        }
+        return result;
+    }
+
+     */
 
 
     private class ListViewExampleClickListener implements AdapterView.OnItemClickListener {
